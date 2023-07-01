ff1049e14d6a4cf469c0c8438c6156f9ce3051a8
BaiacuPlays <131478678+BaiacuPlays@users.noreply.github.com>
2023-07-01 14:55:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 $(document).ready(function() {
-    $('#taskForm').on('submit', function(event) {
-      event.preventDefault();
-      let taskInput = $('#taskInput');
-      let taskName = taskInput.val();
-  
-      if (taskName.trim() !== '') {
-        let taskItem = $('<li></li>').text(taskName);
-        $('#taskList').append(taskItem);
-        taskInput.val('');
-      }
-    });
-  
-    $('#taskList').on('click', 'li', function() {
-      $(this).toggleClass('completed');
-    });
-  });
-  
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $("#botao-cancelar").click(function(){
+        $("form").slideUp()
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const endereçodanovaimagem = $('#endereço-imagem-nova').val()
+        const novoitem = $('<li style="display: none"></li>');
+        $(`<img src="${endereçodanovaimagem}" />`).appendTo(novoitem)
+        $(`<div class"overlay-imagem-link"> 
+        <a href= "${endereçodanovaimagem}" target="_blank" title="ver imagem em tamanho real" 
+        ver imagem em tamanho real  
+        </a>
+        </div>`).appendTo(novoitem)
+        $(novoitem).appendTo("ul")
+        $(novoitem).fadeIn(1000);
+        $("#endereço-imagem-nova").val(" ")
+    })
+})
